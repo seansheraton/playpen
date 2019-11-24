4bf3e81546e5bd24fe09329ecce683fe14fcabe9
seansheraton <sean.sheraton@gmail.com>
2019-11-24 18:13:40 +0200

    Added a test

---
+++ Tests/Integration Tests/RabbitMQTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Infrastructure.RabbitMQ;
+using RabbitMQ.Client;
+
+namespace Tests.Integration_Tests
+{
+    public class RabbitMQTest
+    {
+        private IConnection _connection;
+
+        [Fact]
+        public void TestConnection()
+        {
+            var connectionDetail = new RabbitMQConnectionDetail() { HostName = "localhost", UserName = "guest", Password = "guest" };
+
+            var rabbitMQConnectionFactory = new RabbitMQConnectionFactory(connectionDetail);
+            _connection = rabbitMQConnectionFactory.CreateConnection();
+
+            Assert.True(_connection.IsOpen);
+        }
+    }
+}
